6e3334931675f748fd07a1be1424fce5966adbd1
Sajja Dedeepya <dedeepyasajja201@gmail.com>
2025-07-02 11:58:30 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -95,8 +95,6 @@ const Header = () => {
                     <span>Resume</span>
                   </button>
                 </a>
-
-                </a>
               </div>
             </nav>
           </div>
